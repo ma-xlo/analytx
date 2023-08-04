86e0fb9c03930f02246eeb05bbbe1f335845814f
ma-xlo <macorrea-@hotmail.com>
2023-08-04 12:17:22 -0300

    feat: add installation script

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+INSTALLATION_PATH="$HOME/.config"
+PROGRAM_DIR="analytx"
+PROGRAM_PATH="$INSTALLATION_PATH/$PROGRAM_DIR"
+CURRENT_DIR=$(pwd)
+
+function getRemoteRepository() {
+    if [ -d "$PROGRAM_PATH" ]; then
+        echo "Analytx is already installed. Checking updates..."
+        cd "$PROGRAM_PATH" && git pull
+        cd "$CURRENT_DIR" || return
+    else
+        echo "Cloning Analytx repository..."
+        git clone https://github.com/ma-xlo/analytx "$PROGRAM_PATH"
+    fi
+
+}
+
+function giveFilePermission() {
+    chmod +x "$PROGRAM_PATH/analytx"
+}
+
+function isAnalytxAlreadyInPath() {
+    if ! grep -q "$PROGRAM_PATH" "$HOME/.bashrc"; then
+        echo "Adding Analytx to PATH..."
+        echo "PATH=\$PATH:$PROGRAM_PATH" >> "$HOME/.bashrc"
+        return 0
+    fi
+}
+
+getRemoteRepository
+giveFilePermission
+isAnalytxAlreadyInPath
+
+echo "Installation/update completed."
